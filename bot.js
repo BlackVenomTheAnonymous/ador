ec7f0b0e84835bffe8c17598cd9a49f0f14ac4d9
BlackVenomTheAnonymous <65102543+BlackVenomTheAnonymous@users.noreply.github.com>
2021-06-23 21:44:36 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ instagram : adimas_shadoet
 */
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf('1675749539:AAH46KNMRschhJDMuI5dclpp9FnB0znEaK4')
+const bot = new Telegraf('1861096518:AAGXXC72jgJGURnE4fHsDb7tbBjjbxbysVI')
 const axios = require('axios')
 
 /*********[ Apikey ]*********/
